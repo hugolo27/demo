68380c12bd3be822b7b2a88f3e6c2c1b43ee540e
Hugo López <hugo33lopez@hotmail.com>
2020-06-02 10:36:38 -0400

    Visor JSON con boton de copiado

--- demo/views.py
+++ demo/views.py
@@ -3,6 +3,10 @@ from rest_framework.response import Response
 from rest_framework import status
 import json
 from django.db import models
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.contrib import messages, admin
+from django.utils.html import format_html
 
 
 class Request(models.Model):
@@ -12,6 +16,32 @@ class Request(models.Model):
     class Meta:
         verbose_name = 'Request recibido'
         verbose_name_plural = 'Requests recibidos'
+
+
+class RequestAdmin(admin.ModelAdmin):
+    list_display = ('visor', 'id')
+    list_per_page = 10
+
+    def visor(self, obj):
+        return format_html('<a href="/admin/demo/request/'
+                           'ver_request?id={id}">{msg}</a>', id=obj.id,
+                           msg='VER')
+
+
+
+def ver_request(request):
+    id = request.GET.get('id', False)
+    if not id:
+        messages.add_message(request, messages.ERROR, 'Request no encontrado')
+        return redirect('/admin/demo/request')
+    try:
+        req = Request.objects.get(id=id)
+        return render(request, 'ver_request.html', context={"req": req})
+    except:
+        messages.add_message(request, messages.ERROR, 'Request no encontrado')
+        return redirect('/admin/demo/request')
+
+
 
 class Demo(APIView):
 
--- untitled/urls.py
+++ untitled/urls.py
@@ -14,15 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from demo.views import Demo, Request
+from demo.views import Demo, Request, ver_request, RequestAdmin
 from django.contrib import admin
 from django.urls import path
 
-admin.site.register(Request, admin.ModelAdmin)
+admin.site.register(Request, RequestAdmin)
 admin.site.site_header = "Receptor JSON"
 admin.site.site_title = "Receptor JSON"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(u'notification', Demo.as_view(), name='demo')
+    url(u'notification', Demo.as_view(), name='demo'),
+    url(r'^admin/demo/request/ver_request$', ver_request,
+            name='ver_request'),
 ]
